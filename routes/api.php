eeadec88d07da1a1d6c482bfe0fd63b34cb02add
Artem <artemklinkov@yandex.ru>
2021-10-24 22:09:01 +0300

    backend's 80%

---
+++ routes/api.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post(
+    '/token/create',
+    [\App\Http\Controllers\ApiTokenController::class, 'createToken']
+);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource(
+        '/hall',
+        \App\Http\Controllers\HallController::class
+    );
+
+    Route::apiResource(
+        '/hall_conf',
+        \App\Http\Controllers\HallConfController::class
+    );
+
+    Route::apiResource(
+        '/seat',
+        \App\Http\Controllers\SeatController::class
+    );
+
+    Route::apiResource(
+        '/price_list',
+        \App\Http\Controllers\PriceListController::class
+    );
+
+    Route::apiResource(
+        '/movie',
+        \App\Http\Controllers\MovieController::class
+    );
+
+    Route::apiResource(
+        '/movie_schedule',
+        \App\Http\Controllers\MovieScheduleController::class
+    );
+});
